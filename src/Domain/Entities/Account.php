fcdee8bc05d97ebd0408387b9ae5a75992dd8d69
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-17 18:09:29 -0300

    wip

--- src/Domain/Entities/Account.php
+++ src/Domain/Entities/Account.php
@@ -9,15 +9,16 @@ use Costa\Entity\ValueObject\Uuid;
 
 class Account extends Data
 {
+    /**
+     * @var PixKey[]
+     */
+    protected array $pixKeys = [];
+
     public function __construct(
         protected string $name,
         protected Uuid $bank,
         protected string $agency,
         protected string $number,
-        /**
-         * @var PixKey[]
-         */
-        protected array $pixKeys = [],
     ) {
         parent::__construct();
     }
