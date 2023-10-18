a139e5dd27fec3cd1ec42e12b72c4064acb18ac1
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-18 16:55:58 -0300

    wip

--- src/Domain/Repository/PixKeyRepositoryInterface.php
+++ src/Domain/Repository/PixKeyRepositoryInterface.php
@@ -12,7 +12,7 @@ interface PixKeyRepositoryInterface
 {
     public function register(PixKey $pixKey): bool;
 
-    public function findKeyByKind(string $key, string $kind): ?PixKey;
+    public function findKeyByKind(string $kind, string $key): ?PixKey;
 
     public function addAccount(Account $account): void;
 
