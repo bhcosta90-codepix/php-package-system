c93e9c7bb167468509f851414971ab08e00a89e0
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-24 10:21:55 -0300

    wip

--- src/Domain/Entities/Transaction.php
+++ src/Domain/Entities/Transaction.php
@@ -15,6 +15,7 @@ class Transaction extends Data
         protected float $value,
         protected PixKey $pixKeyTo,
         protected string $description,
+        protected ?Uuid $transactionReference = null,
         protected StatusTransaction $status = StatusTransaction::PENDING,
         protected ?string $cancelDescription = null,
     ) {
